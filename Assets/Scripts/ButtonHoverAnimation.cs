0484813c76e79c397e3fcfcdc29eef5e1ccc34f3
Хадижат Исакова <seastar30t@gmail.com>
2025-12-10 16:34:24 +0300

    pause menu(w/o settings)

--- Assets/Scripts/ButtonHoverAnimation.cs
+++ Assets/Scripts/ButtonHoverAnimation.cs
@@ -1,23 +1,48 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+using System.Collections;
 
 public class ButtonHoverAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Settings")]
+    [SerializeField] private float scaleFactor = 1.2f;
+    [SerializeField] private float animationDuration = 0.1f;
+
     private TextMeshProUGUI buttonText;
     private Vector3 originalScale;
+    private Coroutine currentCoroutine;
 
     void Start()
     {
         buttonText = GetComponentInChildren<TextMeshProUGUI>();
-        originalScale = buttonText.transform.localScale;
+        originalScale = transform.localScale;
     }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        buttonText.transform.localScale = originalScale * 1.2f;
+        if (currentCoroutine != null) StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(AnimateScale(originalScale * scaleFactor));
     }
+
     public void OnPointerExit(PointerEventData eventData)
     {
-        buttonText.transform.localScale = originalScale;
+        if (currentCoroutine != null) StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(AnimateScale(originalScale));
+    }
+
+    IEnumerator AnimateScale(Vector3 targetScale)
+    {
+        float time = 0;
+        Vector3 startScale = transform.localScale;
+
+        while (time < animationDuration)
+        {
+            time += Time.unscaledDeltaTime;
+            transform.localScale = Vector3.Lerp(startScale, targetScale, time / animationDuration);
+            yield return null;
+        }
+        transform.localScale = targetScale;
     }
 }
+
