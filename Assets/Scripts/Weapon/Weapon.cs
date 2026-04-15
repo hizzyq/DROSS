f3ba4e50fd9e976d69eb103e1b08eca284f22194
payponn <perminovmihail2.5@gmail.com>
2026-04-15 20:54:49 +0300

    fix second level

--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -121,8 +121,7 @@ public class Weapon : MonoBehaviour
 
     private void UpdateWeaponVisuals(bool active)
     {
-        // На старте и при смене состояния не включаем outline автоматически.
-        // Outline должен включаться только через InteractionManager при наведении.
+        
         if (_outline != null)
             _outline.enabled = false;
 
